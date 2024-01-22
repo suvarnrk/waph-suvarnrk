862e8579577ad9523b13f059f74cc7bfdbf4d6b8
suvarnrk <156829325+suvarnrk@users.noreply.github.com>
2024-01-22 16:54:48 -0500

    Update index.c

--- labs/lab1/index.c
+++ labs/lab1/index.c
@@ -3,9 +3,9 @@ int main(){
 	const char *htmlContent = "<!DOCTYPE html> <html> <head> <title> "
 	"Web Application Programming and Hacking </title> </head> <body> "
 	"<h1> student: Ruthvik Suvarnakanti </h1> <p> This is done as a part of LAB assignment 1 using CGI Web Application with C"
-	"</p> </body>";
+	"</p> </body> </html>";
 
-	printf("content-type: text/html/n/n");
+	printf("content-type: text/html\n\n");
 	printf("/%s", htmlContent);
 	return 0;
 }
