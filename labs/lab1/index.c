d9a6c2df079b1071b27aa220ca24e8e9166f53a1
suvarnrk <suvarnrk@mail.uc.edu>
2024-01-22 16:48:34 -0500

    lab 1

---
+++ labs/lab1/index.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main(){
+	const char *htmlContent = "<!DOCTYPE html> <html> <head> <title> "
+	"Web Application Programming and Hacking </title> </head> <body> "
+	"<h1> student: Ruthvik Suvarnakanti </h1> <p> This is done as a part of LAB assignment 1 using CGI Web Application with C"
+	"</p> </body>";
+
+	printf("content-type: text/html/n/n");
+	printf("/%s", htmlContent);
+	return 0;
+}
