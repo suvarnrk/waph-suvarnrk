6d4a86fb44f653a4b24d30bc768a9c3b8d928b31
suvarnrk <suvarnrk@mail.uc.edu>
2024-02-07 15:44:55 -0500

    misc

--- labs/lab1/echo.php
+++ labs/lab1/echo.php
@@ -1,6 +1,6 @@
 <?php
 $inputData =$_REQUEST["input"];
-echo "The input from the request is <strong>" . $inputData ."</strong>.<br>";
+echo "The input from the request is <strong>" . $inputData . "</strong>.<br>";
 if(empty($_REQUEST["input"])){
   exit("please enter the input field 'input'");
   
