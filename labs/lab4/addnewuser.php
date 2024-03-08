a25a78fae3c1f40d9e368196eeec2874509f41be
suvarnrk <suvarnrk@mail.uc.edu>
2024-03-08 03:07:12 +0000

    updated

---
+++ labs/lab4/addnewuser.php
@@ -0,0 +1,28 @@
+<?php 
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+	if(isset($username) and isset($password)){
+		//echo "Debug> got username=$username;got password=$password";
+		if(addnewuser($username, $password)){
+			echo "Registration Succeed";
+ 		}else{
+		echo "Registartion Failed!";
+	}
+}
+else{
+	echo " No username/password provided! ";
+}
+	function addnewuser($username, $password){
+		$mysqli=  new mysqli('localhost','suvarnrk','ubuntu','waph');
+		if($mysqli->connect_errno){ 
+			printf("Database connection failed; %s\n", $mysqli->connect_error);
+			return FALSE;
+		}
+		$sql = "INSERT INTO users(username,password) VALUES(?,md5(?));";
+		$stmt = $mysqli->prepare($sql);
+		$stmt->bind_param("ss", $username,$password);
+		if($stmt->execute())
+			return TRUE;
+		return FALSE;
+	}
+?>
